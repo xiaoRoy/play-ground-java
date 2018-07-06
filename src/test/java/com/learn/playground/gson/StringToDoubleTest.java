4b2f3bf909ce6cb107f7f43564fed60c3531d235
Jerry Li <xiaoroyg@163.com>
2018-07-06 23:10:05 +0800

    Learned exception testing.

--- src/test/java/com/learn/playground/gson/StringToDoubleTest.java
+++ src/test/java/com/learn/playground/gson/StringToDoubleTest.java
@@ -1,7 +1,12 @@
 package com.learn.playground.gson;
 
+import com.google.gson.JsonSyntaxException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 public class StringToDoubleTest {
     private static final String GIFT_JSON = "{\"name\": \"apple\", \"price\":145.326}";
@@ -19,9 +24,20 @@ public class StringToDoubleTest {
         Assert.assertEquals(145.326, parser.parserGift(GIFT_JSON_STRING_PRICE).getPrice(), 0.0d);
     }
 
-    @Test
+    @Test(expected = JsonSyntaxException.class)
     public void test_parse_gift_long_price_with_string_price(){
         StringToDouble parser = new StringToDouble();
-        Assert.assertEquals(145.326, parser.parseGift(GIFT_JSON_STRING_PRICE).getPrice(), 0.0d);
+        parser.parseGift(GIFT_JSON_STRING_PRICE).getPrice();
+    }
+
+    @Test
+    public void test_parse_gift_long_price_with_string_price_exception(){
+        StringToDouble parser = new StringToDouble();
+        try{
+            parser.parseGift(GIFT_JSON_STRING_PRICE).getPrice();
+            fail("Expect a JsonSyntaxException");
+        } catch (JsonSyntaxException exception){
+            assertThat(exception.getMessage(), is("java.lang.NumberFormatException: Expected a long but was 145.326 at line 1 column 36 path $.price"));
+        }
     }
 }
