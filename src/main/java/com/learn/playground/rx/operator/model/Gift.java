65142d4a30756f8ccb255a15c841e45bf1275dee
Jerry Li <jerli@xogrp.com>
2018-07-06 19:08:36 +0800

    Learn Repeate observable

---
+++ src/main/java/com/learn/playground/rx/operator/model/Gift.java
@@ -0,0 +1,18 @@
+package com.learn.playground.rx.operator.model;
+
+public class Gift {
+
+    private int count;
+
+    public Gift(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
