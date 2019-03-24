d0cd22b92e9d22cf92da24dd01d61a1fec844ab0
Jerry Li <xiaoroyg@163.com>
2019-03-24 19:06:22 +0800

    Learned Test Observer and Test Scheduler.

--- src/main/java/com/learn/playground/rx/test/RxTestingField.java
+++ src/main/java/com/learn/playground/rx/test/RxTestingField.java
@@ -1,8 +1,12 @@
 package com.learn.playground.rx.test;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class RxTestingField {
 
@@ -12,5 +16,16 @@ public class RxTestingField {
     public Observable<String> combineLetterAndNumber(List<String> letters) {
         return Observable.fromIterable(letters)
                 .zipWith(Observable.range(1, 100), (letter, number) -> letter + "-" + number);
+    }
+
+    public Observable<String> combineLetterAndNumberWithError(List<String> letters) {
+        return combineLetterAndNumber(letters)
+                .concatWith(Observable.error(new IOException()));
+    }
+
+    public Observable<String> combineLetterAndNumberHasInterval(List<String> letters, Scheduler scheduler) {
+        Observable<Long> tick = Observable.interval(1, TimeUnit.SECONDS, scheduler);
+        return Observable.fromIterable(letters)
+                .zipWith(tick, (letter, number) -> letter + "-" + number);
     }
 }
--- src/test/java/com/learn/playground/rx/test/TestRxTestingField.java
+++ src/test/java/com/learn/playground/rx/test/TestRxTestingField.java
@@ -2,19 +2,15 @@ package com.learn.playground.rx.test;
 
 import io.reactivex.Observable;
 import io.reactivex.observers.TestObserver;
-import io.reactivex.subscribers.TestSubscriber;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
+import io.reactivex.schedulers.TestScheduler;
 import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.ArgumentMatcher;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Matchers;
-import org.mockito.Mockito;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class TestRxTestingField {
 
@@ -38,6 +34,40 @@ public class TestRxTestingField {
         resultObservable.subscribe(testObserver);
         testObserver.assertComplete();
         testObserver.assertNoErrors();
+        testObserver.assertValueCount(5);
+    }
+
+    @Test
+    public void test_combineLetterAndNumber_use_testSubscriber_onError() {
+        RxTestingField rxTestingField = new RxTestingField();
+        Observable<String> resultObservable =
+                rxTestingField.combineLetterAndNumberWithError(Arrays.asList("A", "B", "C", "D", "E"));
+
+        TestObserver<String> testObserver = new TestObserver<>();
+        resultObservable.subscribe(testObserver);
+        testObserver.assertError(IOException.class);
+        testObserver.assertNotComplete();
+    }
+
+    @Test
+    public void test_combineLetterAndNumber_use_testSubscriber_testScheduler() {
+        TestScheduler testScheduler = new TestScheduler();
+        TestObserver<String> testObserver = new TestObserver<>();
+        RxTestingField rxTestingField = new RxTestingField();
+        Observable<String> resultObservable =
+                rxTestingField.combineLetterAndNumberHasInterval(Arrays.asList("A", "B", "C", "D", "E"), testScheduler);
+        resultObservable.subscribe(testObserver);
+
+        testObserver.assertNoValues();
+        testObserver.assertNotComplete();
+
+        testScheduler.advanceTimeBy(1, TimeUnit.SECONDS);
+        testObserver.assertNotComplete();
+        testObserver.assertValueCount(1);
+        testObserver.assertValues("A-0");
+
+        testScheduler.advanceTimeBy(6, TimeUnit.SECONDS);
+        testObserver.assertComplete();
         testObserver.assertValueCount(5);
     }
 }
